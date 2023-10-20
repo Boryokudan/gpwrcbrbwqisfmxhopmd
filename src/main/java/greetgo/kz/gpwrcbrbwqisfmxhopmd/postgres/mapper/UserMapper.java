f3d1e83eb6c5c62296c933fd9a939ec51df6bb8e
ZYermukhamet <z.yermukhamet@outlook.com>
2023-10-20 17:33:16 +0600

    added UserMapper interface

---
+++ src/main/java/greetgo/kz/gpwrcbrbwqisfmxhopmd/postgres/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.mapper;
+
+import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.model.User;
+import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.model.UserDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserDto toDto(User user);
+    User toEntity(UserDto userDto);
+    List<UserDto> toDtoList(List<User> users);
+    List<User> toEntityList(List<UserDto> userDtos);
+}

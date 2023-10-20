c9983dc0a6ac1edbe4faed0353a84e82f2ff7abc
ZYermukhamet <z.yermukhamet@outlook.com>
2023-10-20 18:02:52 +0600

    added utility class Filter

---
+++ src/main/java/greetgo/kz/gpwrcbrbwqisfmxhopmd/postgres/util/Filter.java
@@ -0,0 +1,10 @@
+package greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Filter {
+    private int limit;
+    private int offset;
+}

db51f46d87dd1db122543338b5d275f76e22c2fd
ZYermukhamet <z.yermukhamet@outlook.com>
2023-10-20 20:28:52 +0600

    defined custom @Query methods

--- src/main/java/greetgo/kz/gpwrcbrbwqisfmxhopmd/postgres/repository/UserRepository.java
+++ src/main/java/greetgo/kz/gpwrcbrbwqisfmxhopmd/postgres/repository/UserRepository.java
@@ -1,10 +1,25 @@
 package greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.repository;
 
 import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    User findByPrimaryPhoneNumberOrSecondaryPhoneNumber(String primary, String secondary);
+
+    @Query("SELECT u FROM User u WHERE u.primaryPhoneNumber = :phoneNumber OR u.secondaryPhoneNumber = :phoneNumber")
+    User findUserByPrimaryOrSecondaryPhoneNumber(@Param("phoneNumber") String phoneNumber);
+
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.primaryPhoneNumber = :phoneNumber OR u.secondaryPhoneNumber = :phoneNumber")
+    void deleteUserByPrimaryOrSecondaryPhoneNumber(@Param("phoneNumber") String phoneNumber);
 }

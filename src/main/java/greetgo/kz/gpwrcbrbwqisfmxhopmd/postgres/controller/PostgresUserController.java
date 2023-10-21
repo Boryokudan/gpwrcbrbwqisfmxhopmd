2deb51eec108501e9cb1d19591ff17c8f9a72617
ZYermukhamet <z.yermukhamet@outlook.com>
2023-10-21 22:33:09 +0600

    added rest controller for postgres

--- src/main/java/greetgo/kz/gpwrcbrbwqisfmxhopmd/postgres/controller/PostgresUserController.java
+++ src/main/java/greetgo/kz/gpwrcbrbwqisfmxhopmd/postgres/controller/PostgresUserController.java
@@ -1,11 +1,12 @@
 package greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.controller;
 
-import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.service.UserService;
+import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.service.UserDtoService;
+import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.util.Filter;
 import greetgo.kz.gpwrcbrbwqisfmxhopmd.postgres.util.UserDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,10 +15,42 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostgresUserController {
 
-    private final UserService userService;
+    private final UserDtoService userDtoService;
 
-    @GetMapping("/filter")
-    public List<UserDto> getUsers() {
-        return null;
+    @PostMapping("/filter")
+    public ResponseEntity<List<UserDto>> getUsers(@RequestBody Filter filter) {
+        List<UserDto> filteredUsers = userDtoService.getFilteredUserDtos(filter);
+        if (!filteredUsers.isEmpty()) return new ResponseEntity<>(filteredUsers, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
+        UserDto userDto = userDtoService.getUserDtoById(id);
+        if (userDto != null) return new ResponseEntity<>(userDto, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<UserDto> updateUserById(@PathVariable(name = "id") Long id, @RequestBody UserDto updatedUserDto) {
+        UserDto userDto = userDtoService.updateUserDtoById(id, updatedUserDto);
+        if (userDto != null) return new ResponseEntity<>(userDto, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/phone/{phoneNumber}")
+    public ResponseEntity<UserDto> getUserByPhoneNumber(@PathVariable(name = "phoneNumber") String phoneNumber) {
+        UserDto userDto = userDtoService.getUserDtoByPhoneNumber(phoneNumber);
+        if (userDto != null) return new ResponseEntity<>(userDto, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/phone/{phoneNumber}")
+    public ResponseEntity<UserDto> updateUserByPhoneNumber(
+            @PathVariable(name = "phoneNumber") String phoneNumber,
+            @RequestBody UserDto updatedUserDto) {
+        UserDto userDto = userDtoService.updateUserDtoByPhoneNumber(phoneNumber, updatedUserDto);
+        if (userDto != null) return new ResponseEntity<>(userDto, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
